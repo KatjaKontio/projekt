bd2fb530fd908723e31b2f6bb7a02a166d6c5ae9
Katja Kontio <kontiokatja@gmail.com>
2021-01-21 13:02:01 +0100

    routes and navbar added

--- src/components/navigation/desktopnavigation/Desktopnavigation.tsx
+++ src/components/navigation/desktopnavigation/Desktopnavigation.tsx
@@ -1,11 +1,26 @@
 import './Desktopnavigation.css'
-import Logotype from '../../../shared/images/icon.jpg'
+import Logotype from '../../../shared/images/logo.png'
+import { useHistory } from 'react-router-dom'
 
 export const Desktopnavigation = () => {
+    const history = useHistory();
     return (
         <div className='desktopNavigationWrapper'>
-            <img className='navigationLogotype'
-            src={Logotype}></img>
+            <div className="logoWrapper">
+             <img onClick={() => history.push('/home')} className='navigationLogotype'
+                    src={Logotype}/>
             </div>
+
+            <ul>
+                <div className="navWrapper">
+                    <li><a href="/home">Hem</a></li>
+                    <li><a href="">Timer</a></li>
+                    <li><a href="">Övningar</a></li>
+                    <li><a  onClick={() => history.push('/about')}>Om oss</a></li>
+                </div>
+            </ul>
+
+
+        </div>
     )
 }
